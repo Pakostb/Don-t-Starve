3215f229864e0a49c157f8a14311b99caf14224b
Pakostb <sakurah76833@gmail.com>
2026-03-17 13:53:32 +0300

    Add drawSpider function to draw a red circle

--- js/ai.js
+++ js/ai.js
@@ -11,3 +11,11 @@ function hello() {
 function bye() {
     console.log("AI ВСЕ!");
 }
+
+window.drawSpider = function(ctx, x, y) {
+
+    ctx.fillStyle = 'red';
+    ctx.beginPath();
+    ctx.arc(x, y, 15, 0, Math.PI * 2);
+    ctx.fill();
+}
