39807ad13a8a3e5e493f008a5005e5aa26144b50
AnaliniDebozhir228 <anya.maw16@gmail.com>
2026-03-24 14:01:06 +0300

    Update effects.js

--- js/effects.js
+++ js/effects.js
@@ -18,3 +18,24 @@ window.drawVerticalSparks = function(ctx, x, y) {
     ctx.lineTo(x, y + 20);
     ctx.stroke();
 }
+
+// Функция для рисования горизонтальных искр
+window.drawHorizontalSparks = function(ctx, x, y) {
+    // TODO: Две жёлтые линии: влево и вправо от точки
+    // Толщина линий: 2px
+    // 👇 Твой код здесь
+    ctx.strokeStyle = 'yellow';
+    ctx.lineWidth = 2;
+    
+    // Влево
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x - 20, y);
+    ctx.stroke();
+    
+    // Вправо
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + 20, y);
+    ctx.stroke();
+}
